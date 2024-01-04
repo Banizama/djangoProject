05bfaa5ef8ee7e41d181e87d805be2643db3f68b
Banizama <ovsiknazik@gmail.com>
2024-01-04 21:04:41 +0200

    add view for registration page

--- Project/Project_app/views.py
+++ Project/Project_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, FormView
+from .forms import RegUserForm
+from .models import User
 
 
 class HomePage(TemplateView):
     template_name = 'home_page.html'
+
+
+# def register(request):
+#     form = RegUserForm()
+#     return render(request, 'reg_page.html', {'form': form})
+
+
+class RegPage(CreateView):
+    template_name = 'reg_page.html'
+    success_url = reverse_lazy('home')
+    form_class = RegUserForm
