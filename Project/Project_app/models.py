c89e2d1e4e0fc899b815580838624effecfd5702
Banizama <ovsiknazik@gmail.com>
2024-01-04 14:54:21 +0200

    add Post table

--- Project/Project_app/models.py
+++ Project/Project_app/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Post(models.Model):
+    description = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='static/images')
+
 
 class Abstract(models.Model):
     username = models.CharField(max_length=50)
@@ -14,7 +18,11 @@ class Abstract(models.Model):
     
 class User(Abstract):
     password = models.CharField(max_length=100)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 
 class Friend(Abstract):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
