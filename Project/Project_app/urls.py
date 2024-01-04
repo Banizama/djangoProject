f88b0c980c427553dbc3429bf455b85e50a72f64
Banizama <ovsiknazik@gmail.com>
2024-01-04 21:05:34 +0200

    add urlpattern for reg page

--- Project/Project_app/urls.py
+++ Project/Project_app/urls.py
@@ -1,6 +1,7 @@
-from .views import HomePage
+from .views import HomePage, RegPage
 from django.urls import path
 
 urlpatterns = [
-    path('', HomePage.as_view())
+    path('', HomePage.as_view(), name='home'),
+    path('register', RegPage.as_view(), name='register')
 ]
