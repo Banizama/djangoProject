12768f4361815bc1b9221437b5c175914a047ceb
Banizama <ovsiknazik@gmail.com>
2024-01-04 21:08:18 +0200

    rewrite some columns

--- Project/Project_app/models.py
+++ Project/Project_app/models.py
@@ -1,28 +1,28 @@
 from django.db import models
 
-# Create your models here.
-
-class Post(models.Model):
-    description = models.CharField(max_length=200)
-    img = models.ImageField(upload_to='static/images')
-
 
 class Abstract(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
-    image = models.ImageField(upload_to='static/images')
+    image = models.ImageField(upload_to='static/images', default='static/images/default_user_img.jpg')
     date_of_birth = models.DateTimeField()
-    class Meta():
+
+    class Meta:
         abstract = True
 
     
 class User(Abstract):
     password = models.CharField(max_length=100)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 
 class Friend(Abstract):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='static/images')
 
 
 
