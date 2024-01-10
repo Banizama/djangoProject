65856f1207e7ed034dce51ce8bcb91f7ed757227
Banizama <ovsiknazik@gmail.com>
2024-01-10 22:45:43 +0200

    made views for creating a post, logout and current user page

--- Project/Project_app/views.py
+++ Project/Project_app/views.py
@@ -1,10 +1,13 @@
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView, FormView
-from .forms import RegUserForm
+from .forms import RegUserForm, PostForm
 from .models import User
+from django.contrib import messages
 
 
 class HomePage(TemplateView):
@@ -28,4 +31,33 @@ class LoginPage(LoginView):
     form_class = AuthenticationForm
 
     def get_success_url(self):
-        return reverse_lazy('home')
+        return reverse_lazy('cur_user_page')
+
+
+class CreatePost(CreateView):
+    template_name = 'cur_user_page.html'
+    success_url = reverse_lazy('cur_user_page')
+    form_class = PostForm
+
+
+# @login_required
+# class CurUserPage(TemplateView):
+#     template_name = 'cur_user_page.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['user'] = self.request
+
+@login_required
+def cur_user_page(request):
+    return render(request, 'cur_user_page.html')
+
+
+def logout_user(request):
+    logout(request)
+    # messages.success(request, ("You've been succesfully logged out"))
+    return redirect('home')
+
+
+
+
