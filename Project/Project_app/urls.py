39b372ad4ab409485780b0108fc79ebd89dc96b8
Banizama <ovsiknazik@gmail.com>
2024-01-10 22:44:37 +0200

    assign urlpatterns for logout and current user page

--- Project/Project_app/urls.py
+++ Project/Project_app/urls.py
@@ -1,8 +1,12 @@
-from .views import HomePage, RegPage, LoginPage
+from django.contrib.auth.decorators import login_required
+
+from .views import HomePage, RegPage, LoginPage, cur_user_page, logout_user
 from django.urls import path
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
     path('register', RegPage.as_view(), name='register'),
-    path('login', LoginPage.as_view(), name='login')
+    path('login', LoginPage.as_view(), name='login'),
+    path('logout', logout_user, name='logout'),
+    path('cur_user_page', login_required(cur_user_page), name='cur_user_page')
 ]
