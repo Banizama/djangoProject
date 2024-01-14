677e3d21cdd6e3da3ea28a92e826a1ae92f8f818
Banizama <ovsiknazik@gmail.com>
2024-01-14 17:13:00 +0200

    create PostPage view

--- Project/Project_app/views.py
+++ Project/Project_app/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView, FormView
 from .forms import RegUserForm, PostForm
-from .models import User
+from .models import User, Post
 from django.contrib import messages
 
 
@@ -34,10 +34,15 @@ class LoginPage(LoginView):
         return reverse_lazy('cur_user_page')
 
 
-class CreatePost(CreateView):
-    template_name = 'cur_user_page.html'
-    success_url = reverse_lazy('cur_user_page')
-    form_class = PostForm
+class PostPage(TemplateView):
+    template_name = 'post_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        posts = Post.objects.filter(user=self.kwargs['id'])
+        print(posts)
+        context['posts'] = posts
+        print(context)
 
 
 # @login_required
@@ -48,14 +53,26 @@ class CreatePost(CreateView):
 #         context = super().get_context_data(**kwargs)
 #         context['user'] = self.request
 
+
 @login_required
 def cur_user_page(request):
-    return render(request, 'cur_user_page.html')
+    print(request.user.id)
+    cur_user_post = Post.objects.filter(user=request.user.id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            img = form.cleaned_data['img']
+            description = form.cleaned_data['description']
+            post = Post(img=img, description=description, user=request.user)
+            post.save()
+            return redirect('/')
+    else:
+        form = PostForm()
+    return render(request, 'cur_user_page.html', {'form': form, 'posts': cur_user_post})
 
 
 def logout_user(request):
     logout(request)
-    # messages.success(request, ("You've been succesfully logged out"))
     return redirect('home')
 
 
