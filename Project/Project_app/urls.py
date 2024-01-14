31f0cd4bdf04557f68bd1d66c41c6b815e6ef952
Banizama <ovsiknazik@gmail.com>
2024-01-14 17:11:29 +0200

    create urlpattern for post page

--- Project/Project_app/urls.py
+++ Project/Project_app/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 
-from .views import HomePage, RegPage, LoginPage, cur_user_page, logout_user
+from .views import HomePage, RegPage, LoginPage, cur_user_page, logout_user, PostPage
 from django.urls import path
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('register', RegPage.as_view(), name='register'),
     path('login', LoginPage.as_view(), name='login'),
     path('logout', logout_user, name='logout'),
-    path('cur_user_page', login_required(cur_user_page), name='cur_user_page')
+    path('cur_user_page', login_required(cur_user_page), name='cur_user_page'),
+    path('post/<int:id>', PostPage.as_view(), name='post_page')
 ]
