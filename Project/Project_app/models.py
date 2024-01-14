25112f499520aa581c476f8965d683276b3ff748
Banizama <ovsiknazik@gmail.com>
2024-01-14 17:10:46 +0200

    create comment table

--- Project/Project_app/models.py
+++ Project/Project_app/models.py
@@ -5,24 +5,26 @@ from django.db import models
 class Abstract(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
-    image = models.ImageField(upload_to='static/images', default='static/images/default_user_img.jpg')
+    image = models.ImageField(upload_to='static/images', default='media/images/default_user_img.jpg')
 
     class Meta:
         abstract = True
 
-    
+
 class User(AbstractUser):
     pass
-
-
-class Friend(Abstract):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
-    img = models.ImageField(upload_to='static/images')
+    img = models.ImageField(upload_to='images')
 
 
+class Comment(models.Model):
+    text = models.CharField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
+class Followers(models.Model):
+    pass
