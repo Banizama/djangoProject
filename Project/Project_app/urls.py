fd3447db0f6b56d2555ada5a1e8d2a733ffff37a
Banizama <ovsiknazik@gmail.com>
2024-01-18 15:11:00 +0200

    remove pattern for search view

--- Project/Project_app/urls.py
+++ Project/Project_app/urls.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from .views import HomePage, RegPage, LoginPage, cur_user_page, logout_user, PostPage
+from .views import home_page, RegPage, LoginPage, cur_user_page, logout_user, PostPage
 from django.urls import path
 
 urlpatterns = [
-    path('', HomePage.as_view(), name='home'),
-    path('register', RegPage.as_view(), name='register'),
-    path('login', LoginPage.as_view(), name='login'),
-    path('logout', logout_user, name='logout'),
-    path('cur_user_page', login_required(cur_user_page), name='cur_user_page'),
-    path('post/<int:id>', PostPage.as_view(), name='post_page')
+    path('', home_page, name='home'),
+    path('register/', RegPage.as_view(), name='register'),
+    path('login/', LoginPage.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('cur_user_page/', login_required(cur_user_page), name='cur_user_page'),
+    path('post/<int:id>/', PostPage.as_view(), name='post_page'),
 ]
