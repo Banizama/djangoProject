9ae96959cd2222275d5647d1efad6860a556fe62
Banizama <ovsiknazik@gmail.com>
2024-01-21 22:40:25 +0200

    write js for follows and likes

--- Project/Project_app/static/scripts/Scripts.js
+++ Project/Project_app/static/scripts/Scripts.js
@@ -17,3 +17,24 @@ function subscribe(){
     $(document).ready(function(){
         subscribe()
     })
+
+
+function like(){
+    $('#like_btn').click(function(){
+        $.ajax($('#like_btn').data('url'), {
+            'type': 'POST',
+            'async': true,
+            'dataType': 'json',
+            'data': {
+                    'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
+                    },
+                    'success': function(data){
+                        document.getElementById('like_btn').innerHTML = data['like'];
+                        document.getElementById('likes').innerHTML = data['likes'];
+                    }
+            })
+        })
+    }
+    $(document).ready(function(){
+        subscribe()
+    })
