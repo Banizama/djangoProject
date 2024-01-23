072ae1aa4b0dee0078fe8cca28e788a85e5bfbdf
Banizama <ovsiknazik@gmail.com>
2024-01-23 21:57:57 +0200

    renamed file, from Scripts.js to subscribe.js

--- Project/Project_app/static/scripts/subscribe.js
+++ Project/Project_app/static/scripts/subscribe.js
@@ -19,22 +19,3 @@ function subscribe(){
     })
 
 
-function like(){
-    $('#like_btn').click(function(){
-        $.ajax($('#like_btn').data('url'), {
-            'type': 'POST',
-            'async': true,
-            'dataType': 'json',
-            'data': {
-                    'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
-                    },
-                    'success': function(data){
-                        document.getElementById('like_btn').innerHTML = data['like'];
-                        document.getElementById('likes').innerHTML = data['likes'];
-                    }
-            })
-        })
-    }
-    $(document).ready(function(){
-        subscribe()
-    })
