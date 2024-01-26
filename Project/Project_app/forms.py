4ba1148d752cdf8c85029cdb4eab0d0ee06f278d
Banizama <ovsiknazik@gmail.com>
2024-01-26 21:58:49 +0200

    add classes for inputs

--- Project/Project_app/forms.py
+++ Project/Project_app/forms.py
@@ -1,24 +1,42 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from .models import User, Post, Comment
 
 
 
+
+
 class RegUserForm(UserCreationForm):
     username = forms.CharField(help_text='')
-    email = forms.CharField(help_text='', widget=forms.EmailInput())
-    password1 = forms.CharField(help_text='', widget=forms.PasswordInput())
-    password2 = forms.CharField(help_text='', widget=forms.PasswordInput())
+    email = forms.CharField(help_text='')
+    password1 = forms.PasswordInput()
+    password2 = forms.PasswordInput()
 
     class Meta:
         model = get_user_model()
         fields = ('username', 'email', 'password1', 'password2')
+        widgets = {'username': forms.TextInput(attrs={'id': 'username'}),
+                   'email': forms.EmailInput(attrs={'id': 'email'}),
+                   'password1': forms.PasswordInput(attrs={'id': 'password1'}),
+                   'password2': forms.PasswordInput(attrs={'id': 'password2'})}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'input'
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['email'].widget.attrs['class'] = 'input'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['password1'].widget.attrs['class'] = 'input-password1'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['class'] = 'input-password2'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Repeat password'
 
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password1'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t equal')
+            raise forms.ValidationError('Passwords aren\'t equal')
         return cd['password1']
 
     def clean_email(self):
@@ -27,6 +45,12 @@ class RegUserForm(UserCreationForm):
             raise forms.ValidationError('This email is already used')
         return email
 
+
+# class MessageForm(forms.ModelForm):
+#     class Meta:
+#         model = Message
+#         fields = ('text',)
+#         widgets = {'text': forms.Textarea(attrs={'id': 'message'})}
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -38,3 +62,4 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
+        widgets = {'text': forms.TextInput(attrs={'id': 'comment'})}
