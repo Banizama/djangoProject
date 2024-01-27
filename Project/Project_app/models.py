34b4641fc09a422597d56d53c178a64e36e0da18
Banizama <ovsiknazik@gmail.com>
2024-01-27 22:31:20 +0200

    models

--- Project/Project_app/models.py
+++ Project/Project_app/models.py
@@ -5,14 +5,13 @@ from django.db import models
 class Abstract(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
-    image = models.ImageField(upload_to='static/images', default='media/images/default_user_img.jpg')
 
     class Meta:
         abstract = True
 
 
 class User(AbstractUser):
-    pass
+    image = models.ImageField(upload_to='avatars', default=None)
 
 
 class Follow(models.Model):
@@ -32,8 +31,3 @@ class Comment(models.Model):
     text = models.CharField(max_length=300)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-
-class Chat(models.Model):
-    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1')
-    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user2')
